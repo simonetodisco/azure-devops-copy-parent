962ae99bba1e42b116df67af929dcb7ebb8e9205
Simone <simo.tod93@gmail.com>
2021-03-23 00:40:05 +0100

    hub storage refactoring and general improvements

--- src/contributions/hub/hub.tsx
+++ src/contributions/hub/hub.tsx
@@ -2,50 +2,47 @@ import './hub.scss'
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import * as SDK from 'azure-devops-extension-sdk'
-import { CommonServiceIds, IExtensionDataService, IExtensionDataManager } from 'azure-devops-extension-api'
 import { Header, TitleSize } from 'azure-devops-ui/Header'
 import { Page } from 'azure-devops-ui/Page'
 import { Checkbox } from 'azure-devops-ui/Checkbox'
+import { Button } from 'azure-devops-ui/Button'
 import { Spinner, SpinnerSize } from 'azure-devops-ui/Spinner'
+import { SettingsService, ISettingsService } from '../../services/settings'
+import { ISettings, initSettings } from '../../models/settings'
 
 interface State {
-  settings: Settings
+  settings: ISettings
   cmpReady: boolean
+  saveEnabled: boolean
 }
 
-const settingLabels: {[key in keyof Settings]: string} = {
+const settingLabels: {[key in keyof ISettings]: string} = {
   area: 'Area',
   iteration: 'Iteration',
   tags: 'Tags',
 }
 
 class Hub extends React.Component<{}, State> {
-  private _dataManager?: IExtensionDataManager
+  private _settingsService?: ISettingsService
 
   constructor(props: {}) {
     super(props)
     this.state = {
-      settings: {
-        area: false,
-        iteration: false,
-        tags: false,
-      },
+      settings: { ...initSettings() },
       cmpReady: false,
+      saveEnabled: false,
     }
   }
 
   public async componentDidMount() {
     await SDK.init()
-    await SDK.ready()
-    const accessToken = await SDK.getAccessToken()
-    const extDataService = await SDK.getService<IExtensionDataService>(CommonServiceIds.ExtensionDataService)
 
-    this._dataManager = await extDataService.getExtensionDataManager(SDK.getExtensionContext().id, accessToken)
+    this._settingsService = new SettingsService()
+    await this._settingsService.init()
 
-    const settings: Settings = await this._dataManager.getValue('settings')
-
+    const settings = await this._settingsService.getSettings()
     this.setState({
-      settings: settings ? settings : this.state.settings,
+      settings: { ...settings },
       cmpReady: true,
     })
   }
@@ -53,23 +50,30 @@ class Hub extends React.Component<{}, State> {
   private async onSettingChange (
     event: React.MouseEvent<HTMLElement> | React.KeyboardEvent<HTMLElement>,
     value: boolean,
-    id: keyof Settings
+    id: keyof ISettings
   ) {
-    if (this._dataManager) {
-      const settings = {
-        ...this.state.settings,
-        [id]: value
-      }
+    const settingsToUpdate = {
+      ...this.state.settings,
+      [id]: value
+    }
 
-      await this._dataManager.setValue('settings', settings)
+    if (settingsToUpdate.iteration) settingsToUpdate.area = true
+    this.setState({
+      settings: {
+        ...settingsToUpdate,
+      },
+      saveEnabled: true,
+    })
+  }
+
+  async onSaveHandler () {
+    if (this._settingsService) {
+      const updatedSettings = await this._settingsService.setSettings(this.state.settings)
 
       this.setState({
-        settings: {
-          ...settings,
-        }
+        settings: { ...updatedSettings },
+        saveEnabled: false,
       })
-    } else {
-      console.error('State not saved')
     }
   }
 
@@ -86,16 +90,27 @@ class Hub extends React.Component<{}, State> {
       <Page className="flex-grow">
         <Header title="Copy Parent" titleSize={TitleSize.Large} />
         <div className="page-content">
-          Select the fields you want to copy from the work item's parent. The updates will happen just if the changes are made through the UI.
-        </div>
-        <div className="page-content flex-column">
-          {Object.keys(this.state.settings).map((k: string) => <Checkbox
-            key={k}
-            id={k}
-            label={settingLabels[k as keyof Settings]}
-            checked={this.state.settings[k as keyof Settings]}
-            onChange={(event, value) => this.onSettingChange(event, value, k as keyof Settings)}
-          />)}
+          <div>
+            Select the fields you want to copy from the work item's parent. The updates will happen just if the changes are made through the UI.
+          </div>
+          <div className="hub-margin-vertical flex-column">
+            {Object.keys(this.state.settings).map((k: string) => <Checkbox
+              key={k}
+              id={k}
+              label={settingLabels[k as keyof ISettings]}
+              checked={this.state.settings[k as keyof ISettings]}
+              onChange={(event, value) => this.onSettingChange(event, value, k as keyof ISettings)}
+              disabled={k === 'area' && this.state.settings.iteration}
+            />)}
+          </div>
+          <div>
+            <Button
+              text="Save"
+              primary={true}
+              onClick={() => this.onSaveHandler()}
+              disabled={!this.state.saveEnabled}
+            />
+          </div>
         </div>
       </Page>
     )
